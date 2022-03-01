0b9bf9fcf322f6e4710497c598ceeb4e4bf5f29d
regevson <juwal.regev@hotmail.com>
2022-03-01 17:50:59 +0100

    Add cors-handler

--- backend/src/main/java/at/ahmacademy/ahmnet/configs/WebSecurityConfig.java
+++ backend/src/main/java/at/ahmacademy/ahmnet/configs/WebSecurityConfig.java
@@ -1,10 +1,14 @@
 package at.ahmacademy.ahmnet.configs;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,9 +20,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import at.ahmacademy.ahmnet.filters.CustomAuthenticationFilter;
 import at.ahmacademy.ahmnet.filters.CustomAuthorizationFilter;
+
+
 
 /**
  * Spring configuration for web security.
@@ -35,11 +43,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     DataSource dataSource;
 
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.addAllowedMethod(HttpMethod.TRACE);
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "OPTIONS"));
+
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
-        http.csrf().disable();
+	// set 'Access-Control-Allow-Origin' to '*' globally
+        //http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+
+	http.cors().and()
+        .csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
         http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   devServer: {
-    proxy: 'http://localhost:8080'
+    proxy: 'http://localhost:8080',
+    disableHostCheck: true
   },
 
   transpileDependencies: [
